86df14c68e2e5a82bd7f16c54ce23e5de9c9f7bc
Saibal Dey <cloudguy2020@gmail.com>
2020-07-02 14:35:56 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,7 +4,8 @@ mkdir /data
 mkdir /data/jenkinsprod
 
 # install pre-requisites
-yum install openjdk-8-jdk -y
+# apt install openjdk-8-jdk -y # for Ubuntu 
+yum install java-1.8.0-openjdk -y # for centos
 
 # download the jenkins war file
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
